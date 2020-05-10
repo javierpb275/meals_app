8e857be1f23637fc0ee6cbf01c1a1396cb3b8209
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-05-10 19:16:47 +0200

    first commit

---
+++ lib/pages/category_meals_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/constants/dummy_categories.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_item.dart';
+
+class CategoryMealsScreen extends StatefulWidget {
+  static const ROUTE_NAME = '/category-meals';
+
+  final List<Meal> availableMeals;
+
+  CategoryMealsScreen(this.availableMeals);
+
+  @override
+  _CategoryMealsScreenState createState() => _CategoryMealsScreenState();
+}
+
+class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
+  String categoryTitle;
+  List<Meal> displayedMeals;
+  bool _loadedInitData = false;
+
+  @override
+  void initState() {
+    
+    super.initState();
+  }
+
+    @override
+  void didChangeDependencies() {
+    if (!_loadedInitData) {
+    final routeArgs = 
+    ModalRoute.of(context).settings.arguments as Map<String, String>;
+    this.categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+    this.displayedMeals = widget.availableMeals.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
+    _loadedInitData = true;
+    }
+    super.didChangeDependencies();
+  }
+
+  void _removeMeal(String mealId) {
+    setState(() {
+      displayedMeals.removeWhere((meal) => meal.id == mealId);
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(categoryTitle),
+      ),
+      body: ListView.builder(itemBuilder: (ctx, index) {
+        return MealItem(
+          id: displayedMeals[index].id,
+          title: displayedMeals[index].title,
+          imageUrl: displayedMeals[index].imageUrl,
+          duration: displayedMeals[index].duration,
+          affordability: displayedMeals[index].affordability,
+          complexity: displayedMeals[index].complexity,
+        );
+      }, itemCount: displayedMeals.length),
+    );
+  }
+}
